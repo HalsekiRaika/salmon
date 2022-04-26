76bf60f610ae06175b93d7a784766c23554f048f
ReiRokusanami0010 <reirokusanami.rdh@gmail.com>
2022-04-27 00:59:56 +0900

    🎨 - Changed from the Newtype struct to a struct using generics.

--- src/ids.rs
+++ src/ids.rs
@@ -1,23 +1,86 @@
+#![allow(dead_code)]
+
+use std::fmt::{Display, Formatter};
+use std::marker::PhantomData;
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Clone, Copy, Eq, PartialEq, Hash, Deserialize, Serialize)]
+#[derive(Debug, Deserialize, Serialize, Clone, Copy, Eq, PartialEq, Hash)]
 #[serde(transparent)]
-pub struct AffiliationId(pub i64);
+pub struct NumId<T> {
+    value: i64,
+    #[serde(skip)]
+    _mark: PhantomData<T>
+}
 
-#[derive(Debug, Clone, Copy, Deserialize, Serialize)]
-#[serde(transparent)]
-pub struct LiverId(pub i64);
+impl<T> NumId<T> {
+    pub fn new(id: i64) -> NumId<T> {
+        Self { value: id, _mark: PhantomData }
+    }
 
-#[derive(Debug, Clone, Eq, PartialEq, Deserialize, Serialize)]
-#[serde(transparent)]
-pub struct ChannelId(pub String);
+    pub fn as_ref(&self) -> &i64 {
+        &self.value
+    }
 
-impl From<ChannelId> for String {
-    fn from(id: ChannelId) -> Self {
-        id.0
+    pub fn breach_extract(&self) -> i64 {
+        self.value
     }
 }
 
-#[derive(Debug, Clone, Deserialize, Serialize)]
+impl<T> Display for NumId<T> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        Display::fmt(&self.value, f)
+    }
+}
+
+#[derive(Debug, Deserialize, Serialize, Clone, Eq, PartialEq, Hash)]
 #[serde(transparent)]
-pub struct VideoId(pub String);
+pub struct StringId<T> {
+    value: String,
+    #[serde(skip)]
+    _mark: PhantomData<T>
+}
+
+impl<T> StringId<T> {
+    pub fn new(id: impl Into<String>) -> StringId<T> {
+        Self { value: id.into(), _mark: PhantomData }
+    }
+
+    pub fn as_ref(&self) -> &str {
+        &self.value
+    }
+}
+
+impl<T> Default for StringId<T> {
+    fn default() -> Self {
+        StringId::new(String::new())
+    }
+}
+
+impl<T> From<StringId<T>> for String {
+    fn from(id: StringId<T>) -> Self {
+        id.value
+    }
+}
+
+#[cfg(test)]
+mod id_test {
+    use serde::{Deserialize, Serialize};
+    use crate::ids::NumId;
+
+    #[derive(Debug, Clone, Eq, PartialEq, Deserialize, Serialize)]
+    struct Entry {
+        id: NumId<Entry>
+    }
+
+    #[test]
+    fn parse_test() {
+        let e = Entry {
+            id: NumId::new(1)
+        };
+
+        let s = serde_json::to_string_pretty(&e).expect("");
+        println!("{}", s);
+        let d: Entry = serde_json::from_str(&s).expect("");
+        assert_eq!(e, d);
+    }
+}
